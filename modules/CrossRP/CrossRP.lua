01e4d3bc7a4e626d640a9d35d9752eb145a3d544
LenweSaralonde <lenwe@lenwe.info>
2019-07-06 19:34:24 +0200

    Implemented foreign player scanning in background to avoid freezings #7
    Fixed mouseover version retrieval when missing

--- modules/CrossRP/CrossRP.lua
+++ modules/CrossRP/CrossRP.lua
@@ -7,13 +7,21 @@ Musician.CrossRP.isReady = false
 local streamingSongId
 local chunkId
 local receivedChunkIds = {}
-local foreigners = {}
+local foreigners = {} -- All players having Musician + CrossRP found on other bands
 local queriedPlayers = {}
 local pendingPlayerQueries = {} -- CrossRP Query messages queue
+
+local activeBands = {} -- All bands having in-range and visible foreigners
+local foreignerScanActiveBands = {}
+local foreignerScanIndex
+local foreignerScanLastDuration -- Last time spent scanning for foreigners during 1 frame (ms)
+local foreignerScanLastAmount -- Last amount of foreigners scanned during 1 frame
 
 local QUERY_RATE = .5
 local QUERY_MAX_TRIES = 2
 local QUERY_RETRY_AFTER = 5
+
+local FOREIGNERS_SCAN_MAX_TIME = .5 -- Max time allowed per frame for foreigners scan (ms)
 
 --- Prints debug message
 -- @param out (boolean) Outgoing message
@@ -76,9 +84,10 @@ function Musician.CrossRP.Init()
 		local myBand = CrossRP.Proto.GetBandFromUnit("player")
 		local isInMyBand = CrossRP.Proto.IsDestLinked(myBand, destination)
 		local isPlayerBandReachable = CrossRP.Proto.SelectBridge(destination, false)
+		local playerHasNoVersion = not(Musician.Registry.PlayerIsRegistered(player)) or Musician.Registry.PlayerIsRegisteredWithNoVersion(player)
 
 		-- Player is not in my band and is not registered: send Query message via CrossRP if possible
-		if not(isInMyBand) and isPlayerBandReachable and not(Musician.Registry.PlayerIsRegistered(player)) then
+		if not(isInMyBand) and isPlayerBandReachable and playerHasNoVersion then
 			local query = queriedPlayers[player]
 			local queryCount = query and query[1] or 0
 			local queryTime = query and query[2] or 0
@@ -92,13 +101,23 @@ function Musician.CrossRP.Init()
 
 	-- Periodically send enqueued Query message
 	C_Timer.NewTicker(QUERY_RATE, function()
+
+		-- Take first player of the list
 		local player = table.remove(pendingPlayerQueries, 1)
 
-		while player and Musician.Registry.PlayerIsRegistered(player) do
+		-- Keep removing players from list if they are already registered with a version number
+		while player and Musician.Registry.PlayerIsRegisteredWithVersion(player) do
 			player = table.remove(pendingPlayerQueries, 1)
 		end
 
-		if player and not(Musician.Registry.PlayerIsRegistered(player)) then
+		-- No more player in the list
+		if not(player) then return end
+
+		-- Player still not registered or without version
+		local playerHasNoVersion = not(Musician.Registry.PlayerIsRegistered(player)) or Musician.Registry.PlayerIsRegisteredWithNoVersion(player)
+
+		-- Send Query message
+		if playerHasNoVersion then
 			local destination = queriedPlayers[player][3]
 
 			debug(true, Musician.Registry.event.query, destination, player, unpack(queriedPlayers[player]))
@@ -134,6 +153,57 @@ function Musician.CrossRP.Init()
 			local guid = UnitGUID(simplePlayerName)
 			Musician.CrossRP.RegisterPlayerFromSource(source, guid)
 		end
+	end)
+
+	--- Scan nearby foreingers on each frame
+	--
+	Musician.CrossRP:RegisterMessage(Musician.Events.Frame, function(event, elapsed)
+
+		-- foreigners list is empty
+		if #foreigners == 0 then
+			activeBands = {}
+			return
+		end
+
+		-- Default values
+		if foreignerScanLastDuration == nil or foreignerScanLastDuration == 0 or foreignerScanLastAmount == nil or foreignerScanLastAmount == 0 then
+			foreignerScanLastDuration = 1
+			foreignerScanLastAmount = 100
+		end
+
+		-- Number of scans last performed during 1ms
+		local scansPerMs = foreignerScanLastDuration / foreignerScanLastAmount
+		local scansToDo = math.ceil(FOREIGNERS_SCAN_MAX_TIME / scansPerMs)
+
+		-- Perform scan
+		local myBand = CrossRP.Proto.GetBandFromUnit("player")
+		if foreignerScanIndex == nil then foreignerScanIndex = 1 end
+		local scans = 0
+		local startTime = debugprofilestop()
+		while (scans < #foreigners) and (scans < scansToDo) do
+			local player = foreigners[foreignerScanIndex]
+			local playerData = Musician.Registry.players[player]
+			local guid = playerData.guid
+			local band = playerData.crossRpBand
+
+			-- Player has CrossRP attributes, is not in my group and is visible (connected)
+			if guid and not(Musician.Utils.PlayerIsInGroup(player)) and Musician.Utils.PlayerGuidIsVisible(guid) then
+				foreignerScanActiveBands[band] = true
+			end
+
+			-- Scan is complete
+			if foreignerScanIndex == #foreigners then
+				foreignerScanIndex = 1
+				activeBands = foreignerScanActiveBands
+				foreignerScanActiveBands = {}
+			else
+				foreignerScanIndex = foreignerScanIndex + 1
+			end
+			scans = scans + 1
+		end
+
+		foreignerScanLastDuration = debugprofilestop() - startTime
+		foreignerScanLastAmount = scans
 	end)
 end
 
@@ -181,24 +251,23 @@ end
 --- Return CrossRP destination for broadcast, based on the presence of nearby foreign players
 -- @return (string)
 function Musician.CrossRP.GetBroadcastDestination()
+
 	local myBand = CrossRP.Proto.GetBandFromUnit("player")
-	local adverseBand = Musician.CrossRP.GetAdverseBand()
-	local destination = nil
-	local player, playerData
-	for _, player in ipairs(foreigners) do
-		-- Player has CrossRP attributes, is not in my group and is visible (connected)
-		local playerData = Musician.Registry.players[player]
-		if playerData.guid and not(Musician.Utils.PlayerIsInGroup(player)) and Musician.Utils.PlayerGuidIsVisible(playerData.guid) then
-			-- Adverse band
-			if CrossRP.Proto.IsDestLinked(playerData.crossRpBand, adverseBand) then
-				destination = adverseBand
-			-- Foreign player
-			elseif not(CrossRP.Proto.IsDestLinked(playerData.crossRpBand, myBand)) then
-				return "all"
-			end
+	local destination = {}
+	local band
+	for band, _ in pairs(activeBands) do
+		if not(CrossRP.Proto.IsDestLinked(band, myBand)) then
+			table.insert(destination, band)
 		end
 	end
-	return destination
+
+	if #destination == 0 then
+		return nil
+	elseif #destination == 1 then
+		return destination[1]
+	else
+		return "all"
+	end
 end
 
 --- Register player from its source string
@@ -301,7 +370,7 @@ function Musician.CrossRP.StreamCompressedSongChunk(compressedChunk)
 	local serializedData = LibDeflate:EncodeForWoWChatChannel(packedChunkId .. compressedChunk)
 
 	-- Send chunk
-	debug(true, Musician.Comm.event.stream, destination, streamingSongId, packedChunkId, #serializedData)
+	debug(true, Musician.Comm.event.stream, destination, streamingSongId, chunkId, #serializedData)
 	CrossRP.Proto.Send(
 		destination,
 		{ Musician.Comm.event.stream, serializedData },
@@ -330,7 +399,7 @@ function Musician.CrossRP.OnSongChunk(source, message, complete)
 		local packedChunk = LibDeflate:DecompressDeflate(string.sub(data, 3))
 		local mode, songId = Musician.Song.UnpackChunkHeader(packedChunk)
 
-		debug(false, type, source, songId, packedChunk, #serializedData)
+		debug(false, type, source, songId, chunkId, #serializedData)
 
 		-- Invalid chunk
 		if mode == nil then
