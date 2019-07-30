6cfdeab3309f880bd6a0b1fc6fadef767391aaee
LenweSaralonde <lenwe@lenwe.info>
2019-07-30 22:22:18 +0200

    The player TRP map pins now blink when playing music

--- modules/totalRP3/totalRP3.lua
+++ modules/totalRP3/totalRP3.lua
@@ -3,6 +3,9 @@ Musician.TRP3 = LibStub("AceAddon-3.0"):NewAddon("Musician.TRP3", "AceEvent-3.0"
 local MODULE_NAME = "TRP3"
 Musician.AddModule(MODULE_NAME)
 
+local playersPlayingMusic = {}
+local IS_PLAYING_TIMEOUT = 3
+
 function Musician.TRP3:OnEnable()
 	if TRP3_API then
 		Musician.Utils.Debug(MODULE_NAME, "Total RP3 module started.")
@@ -12,6 +15,7 @@ function Musician.TRP3:OnEnable()
 		TRP3_API.Events.registerCallback("WORKFLOW_ON_FINISH", function()
 			Musician.TRP3.HookTooltip()
 		end)
+		Musician.TRP3:RegisterMessage(Musician.Events.SongChunk, Musician.TRP3.OnSongChunk)
 	end
 end
 
@@ -59,42 +63,129 @@ function Musician.TRP3.HookNamePlates()
 	end
 end
 
+--- Returns true if the player is currently playing some music
+-- @param player (string)
+-- @return (boolean)
+function Musician.TRP3.IsPlayingMusic(player)
+	if not(playersPlayingMusic[player]) then return false end
+
+	if playersPlayingMusic[player] + IS_PLAYING_TIMEOUT < GetTime() then
+		playersPlayingMusic[player] = nil
+		return false
+	end
+
+	return true
+end
+
+--- OnSongChunk
+--
+function Musician.TRP3.OnSongChunk(event, sender, mode, songId, chunkDuration, playtimeLeft, posY, posX, posZ, instanceID, guid)
+	local now = GetTime()
+
+	playersPlayingMusic[sender] = now
+
+	local playerName, time
+	for playerName, time in pairs(playersPlayingMusic) do
+		if time + IS_PLAYING_TIMEOUT < now then
+			playersPlayingMusic[playerName] = nil
+		end
+	end
+end
+
+--- onPinUpdate
+--
+local function onPinUpdate(self, elapsed)
+	local isPlayingMusic = Musician.TRP3.IsPlayingMusic(self.musicianPlayer)
+	local r, g, b, a = unpack(self.musicianColor)
+
+	if isPlayingMusic then
+		self.musicianBlinkTime = self.musicianBlinkTime + elapsed
+		local blink = abs(1 - 2 * (4 * self.musicianBlinkTime % 1))
+		self.Texture:SetVertexColor(r, g, b, Lerp(.33, 1, blink))
+	elseif self.musicianIsPlayingMusic ~= isPlayingMusic then
+		self.Texture:SetVertexColor(r, g, b, a)
+	end
+
+	self.musicianIsPlayingMusic = isPlayingMusic
+end
+
 --- Hook TRP player map
 --
 function Musician.TRP3.HookPlayerMap()
 
 	if TRP3_PlayerMapPinMixin then
 
+		--- TRP3_PlayerMapPinMixin.GetDisplayDataFromPoiInfo
+		--
 		if TRP3_PlayerMapPinMixin.GetDisplayDataFromPoiInfo then
 			local TRP3_PlayerMapPinMixin_GetDisplayDataFromPoiInfo = TRP3_PlayerMapPinMixin.GetDisplayDataFromPoiInfo
 			TRP3_PlayerMapPinMixin.GetDisplayDataFromPoiInfo = function(self, poiInfo, ...)
 				local displayData = TRP3_PlayerMapPinMixin_GetDisplayDataFromPoiInfo(self, poiInfo, ...)
 
-				displayData.musicianIsRegistered = Musician.Registry.PlayerIsRegistered(poiInfo.sender)
+				local sender = poiInfo.sender
+				displayData.musicianIsRegistered = Musician.Registry.PlayerIsRegistered(sender)
+				displayData.musicianPlayer = sender
+
+				-- Check if the player is actually playing music
+				displayData.musicianIsPlayingMusic = false
+				if playersPlayingMusic[sender] then
+					if playersPlayingMusic[sender] + IS_PLAYING_TIMEOUT < GetTime() then
+						playersPlayingMusic[sender] = nil
+					else
+						displayData.musicianIsPlayingMusic = true
+					end
+				end
 
 				-- Slightly raise priority if the player has no special relationship with this one
 				if displayData.musicianIsRegistered and displayData.categoryPriority == -1 then
-					displayData.categoryPriority = -.5
+					if displayData.musicianIsPlayingMusic then
+						displayData.categoryPriority = 99
+					else
+						displayData.categoryPriority = -.5
+					end
 				end
 
 				return displayData
 			end
 		end
 
+		--- TRP3_PlayerMapPinMixin.Decorate
+		--
 		if TRP3_PlayerMapPinMixin.Decorate then
 			local TRP3_PlayerMapPinMixin_Decorate = TRP3_PlayerMapPinMixin.Decorate
 			TRP3_PlayerMapPinMixin.Decorate = function(self, displayData, ...)
 
 				local newDisplayData = Mixin({}, displayData)
+				local isPlayingMusic = false
 
 				-- Append note icon to player name and replace pin texture by a musical note
 				if displayData.musicianIsRegistered then
-					newDisplayData.playerName = newDisplayData.playerName .. " " .. Musician.Utils.GetChatIcon(Musician.IconImages.Note)
+					local icon
+
+					isPlayingMusic = Musician.TRP3.IsPlayingMusic(displayData.musicianPlayer)
+					if isPlayingMusic then
+						icon = Musician.Utils.GetChatIcon(Musician.IconImages.Note, 1, .82, 0)
+					else
+						icon = Musician.Utils.GetChatIcon(Musician.IconImages.Note)
+					end
+
+					newDisplayData.playerName = newDisplayData.playerName .. " " .. icon
 					self.Texture:SetTexture("Interface\\AddOns\\Musician\\ui\\textures\\map-pin.blp")
 					self.HighlightTexture:SetTexture("Interface\\AddOns\\Musician\\ui\\textures\\map-pin-highlight.blp")
 				end
 
+				-- Decorate
 				TRP3_PlayerMapPinMixin_Decorate(self, newDisplayData, ...)
+
+				-- Attach OnUpdate script if registered
+				if displayData.musicianIsRegistered then
+					self.musicianBlinkTime = 0
+					self.musicianColor = { self.Texture:GetVertexColor() }
+					self.musicianPlayer = displayData.musicianPlayer
+					self:SetScript("OnUpdate", onPinUpdate)
+				else
+					self:SetScript("OnUpdate", nil)
+				end
 			end
 		end
 	end
