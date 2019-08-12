029025739c50fb166ce4979ed76f489577f332cb
LenweSaralonde <lenwe@lenwe.info>
2019-08-12 17:04:08 +0200

    Added polyfill for retail UI functions

---
+++ ui/MusicianWidgetsClassic.lua
@@ -0,0 +1,14 @@
+--- WoW Classic polyfill
+--
+
+-- A bit ugly, we want the talent frame to display a dialog box in certain conditions.
+UIPanelCloseButton_OnClick = _G["UIPanelCloseButton_OnClick"] or function(self)
+	local parent = self:GetParent();
+	if parent then
+		if parent.onCloseCallback then
+			parent.onCloseCallback(self);
+		else
+			HideUIPanel(parent);
+		end
+	end
+end
