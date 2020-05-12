5ee5724d693bae2f010eb8664afdca5f363c9893
LenweSaralonde <lenwe@lenwe.info>
2020-05-12 21:30:07 +0200

    Generate LDoc for Classic

--- ui/MusicianWidgetsClassic.lua
+++ ui/MusicianWidgetsClassic.lua
@@ -1,5 +1,5 @@
 --- WoW Classic polyfill
---
+-- @module Musician.WidgetsClassic
 
 -- A bit ugly, we want the talent frame to display a dialog box in certain conditions.
 UIPanelCloseButton_OnClick = _G["UIPanelCloseButton_OnClick"] or function(self)
