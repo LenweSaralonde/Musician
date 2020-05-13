6e8807c017468bf5b885479ef42ee55c5b55bb53
LenweSaralonde <lenwe@lenwe.info>
2020-05-13 21:26:20 +0200

    Mutualized ElvUI nameplate fixes with Classic

--- modules/ElvUI/ElvUI.lua
+++ modules/ElvUI/ElvUI.lua
@@ -7,6 +7,15 @@ Musician.ElvUI = LibStub("AceAddon-3.0"):NewAddon("Musician.ElvUI", "AceEvent-3.
 local MODULE_NAME = "ElvUI"
 Musician.AddModule(MODULE_NAME)
 
+local function updateEvlUI_Nameplate(self, ElvNamePlate)
+	C_Timer.After(0, function()
+		if ElvNamePlate then
+			local namePlate = ElvNamePlate:GetParent()
+			Musician.NamePlates.AddNoteIcon(namePlate, ElvNamePlate.Name)
+		end
+	end)
+end
+
 --- OnEnable
 --
 function Musician.ElvUI:OnEnable()
@@ -17,10 +26,14 @@ function Musician.ElvUI:OnEnable()
 		local E, L, V, P, G = unpack(ElvUI) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 		local NP = E:GetModule("NamePlates")
 
-		if NP.StyleFilterUpdate == nil then
-			Musician.Utils.Debug(MODULE_NAME, "ElvUI NP.StyleFilterUpdate function not found")
+		if NP.StyleFilterUpdate == nil or NP.SetupTarget == ni then
+			Musician.Utils.Debug(MODULE_NAME, "Could not hook ElvUI functions.")
 			return
 		end
+
+		-- Add musical note icon next to player name
+		hooksecurefunc(NP, "Update_Name", updateEvlUI_Nameplate)
+		hooksecurefunc(NP, "SetupTarget", updateEvlUI_Nameplate)
 
 		-- Add musical note icon next to player name
 		hooksecurefunc(NP, "StyleFilterUpdate", function(_, ElvNamePlate, event)
