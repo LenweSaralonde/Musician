b193160eb904f75fe7e1853e40289e5e1ee02005
LenweSaralonde <lenwe@lenwe.info>
2021-02-13 19:47:58 +0100

    Added links in web converter + style tweaks

--- MidiConvert/locale/default.js
+++ MidiConvert/locale/default.js
@@ -1,13 +1,12 @@
 MUSICIAN_MSG.title = "Musician – MIDI Import";
-MUSICIAN_MSG.header = "Import a MIDI file in Musician";
-MUSICIAN_MSG.instructions = "Drop a MIDI file here.";
+MUSICIAN_MSG.header = "Import a MIDI file into Musician";
+MUSICIAN_MSG.instructions = "Click or drop a MIDI file here.";
 MUSICIAN_MSG.copy = "Copy";
 MUSICIAN_MSG.copySuccess = "Copied into clipboard";
 MUSICIAN_MSG.copyError = "Unable to copy";
 MUSICIAN_MSG.unsupportedBrowser = "Unsupported Browser";
 MUSICIAN_MSG.dowload = "Download Musician addon for World of Warcraft";
 MUSICIAN_MSG.dowloadVersion = "Download Musician add-on v{version} for World of Warcraft and WoW Classic";
-MUSICIAN_MSG.windowBackgroundImage = "musician-window-bg-en.png";
 MUSICIAN_MSG.joinDiscord = "Join our Discord community";
 MUSICIAN_MSG.joinBattleNet = "Join our Battle.net group";
 MUSICIAN_MSG.becomeAPatron = "Become a patron to support the project";
