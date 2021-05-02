5909851f145bb6b93379b4bcbfb2e11016fb395a
LenweSaralonde <lenwe@lenwe.info>
2021-05-02 17:38:44 +0200

    Refreshed localization

--- locale/Musician.zh.lua
+++ locale/Musician.zh.lua
@@ -1,9 +1,26 @@
-local msg = Musician.InitLocale('zh', "中文", 'zhCN', 'zhTW')
+------------------------------------------------------------------------
+-- Please read the localization guide in the Wiki:
+-- https://github.com/LenweSaralonde/Musician/wiki/Localization
+--
+-- * Commented out msg lines need to be translated.
+-- * Do not translate anything on the left hand side of the = sign.
+-- * Do not translate placeholders in curly braces ({variable}).
+-- * Keep the text as a single line. Use \n for carriage return.
+-- * Escape double quotes (") with a backslash (\").
+-- * Check the result in game to make sure your text fits the UI.
+------------------------------------------------------------------------
+
+local msg = Musician.InitLocale("zh", "中文", "zhTW", "zhCN")
 
 local Instrument = Musician.MIDI_INSTRUMENTS
 local Percussion = Musician.MIDI_PERCUSSIONS
 local KEY = Musician.KEYBOARD_KEY
 
+------------------------------------------------------------------------
+---------------- ↑↑↑ DO NOT EDIT THE LINES ABOVE ! ↑↑↑  ----------------
+------------------------------------------------------------------------
+
+--- Main frame controls
 msg.PLAY = "播放"
 msg.STOP = "停止"
 msg.PAUSE = "暂停"
@@ -15,6 +32,7 @@ msg.EDIT = "编辑"
 msg.MUTE = "静音"
 msg.UNMUTE = "取消静音"
 
+--- Minimap button menu
 msg.MENU_TITLE = "Musician"
 msg.MENU_IMPORT_SONG = "导入并播放一首音乐"
 msg.MENU_PLAY = "播放"
@@ -27,6 +45,7 @@ msg.MENU_SETTINGS = "设定"
 msg.MENU_OPTIONS = "选项"
 msg.MENU_ABOUT = "关于"
 
+--- Chat commands
 msg.COMMAND_LIST_TITLE = "Musician指令："
 msg.COMMAND_SHOW = "显示音乐导入窗口"
 msg.COMMAND_PREVIEW_PLAY = "开始或停止预览音乐"
@@ -41,9 +60,11 @@ msg.COMMAND_LIVE_DEMO_PARAMS = "{ **<上音轨编号>** **<下音轨编号>** ||
 msg.COMMAND_HELP = "显示这条帮助信息"
 msg.ERR_COMMAND_UNKNOWN = "未知的 \"{command}\" 指令。输入 {help} 来获得指令表。"
 
+--- Global error messages
 msg.ERR_CLASSIC_ON_RETAIL = "你正在正式版服务器使用怀旧版Musician插件，请安装正式版的插件。"
 msg.ERR_RETAIL_ON_CLASSIC = "你正在怀旧版服务器使用正式版Musician插件，请安装怀旧版的插件。"
 
+--- Add-on options
 msg.OPTIONS_TITLE = "Musician"
 msg.OPTIONS_SUB_TEXT = "加入Discord社区来帮助我们！ {url}"
 msg.OPTIONS_CATEGORY_EMOTE = "表情"
@@ -68,6 +89,7 @@ msg.OPTIONS_CROSS_RP_SUB_TEXT = "安装Tammya-月亮守卫的Cross RP插件来�
 msg.OPTIONS_CROSS_RP_SUB_TEXT_NO_GATEWAY = "目前暂无可用的Cross RP节点。\n请耐心等待……"
 msg.OPTIONS_CROSS_RP_SUB_TEXT_ACTIVE = "Cross RP连接已在以下位置启动：\n\n{bands}"
 
+--- Tips and Tricks
 msg.TIPS_AND_TRICKS_ENABLE = "启动时显示小贴士。"
 
 msg.TIPS_AND_TRICKS_NAMEPLATES_TITLE = "动画和姓名版"
@@ -79,29 +101,31 @@ msg.TIPS_AND_TRICKS_CROSS_RP_TITLE = "使用Cross RP进行跨阵营/跨服演奏
 msg.TIPS_AND_TRICKS_CROSS_RP_TEXT = "安装Tammya-月亮守卫的Cross RP插件来进行跨服/跨阵营的音乐演奏！"
 msg.TIPS_AND_TRICKS_CROSS_RP_OK = "好的"
 
+--- Welcome messages
 msg.STARTUP = "欢迎使用Musician v{version}."
+msg.PLAYER_COUNT_ONLINE = "现在有{count}个音乐迷在线！"
+msg.PLAYER_COUNT_ONLINE_ONE = "有一个音乐迷在线！"
+msg.PLAYER_COUNT_ONLINE_NONE = "没有在线的音乐迷。"
 
+--- New version notifications
 msg.NEW_VERSION = "一个新的Musician版本发布了！可以在以下链接下载升级：{url}。"
 msg.NEW_PROTOCOL_VERSION = "你的Musician版本过老，已经无法使用。\n请前往以下链接升级\n{url}"
-msg.SHOULD_CONFIGURE_KEYBOARD = "在开始演奏前需要对键盘进行设置。"
 
+--- Player tooltips
 msg.PLAYER_TOOLTIP = "Musician"
 msg.PLAYER_TOOLTIP_VERSION = "Musician v{version}"
 msg.PLAYER_TOOLTIP_VERSION_OUTDATED = " (当前版本已过期)"
 msg.PLAYER_TOOLTIP_VERSION_INCOMPATIBLE = " (当前版本不兼容)"
 msg.PLAYER_TOOLTIP_PRELOADING = "音效预载中……({progress})"
 
-msg.PLAYER_COUNT_ONLINE = "现在有{count}个音乐迷在线！"
-msg.PLAYER_COUNT_ONLINE_ONE = "有一个音乐迷在线！"
-msg.PLAYER_COUNT_ONLINE_NONE = "没有在线的音乐迷。"
-
+--- Song import
 msg.INVALID_MUSIC_CODE = "音乐代码无效。"
-
 msg.PLAY_A_SONG = "播放音乐"
 msg.IMPORT_A_SONG = "导入音乐"
 msg.PASTE_MUSIC_CODE = "通过以下地址导入你MIDI格式的音乐\n{url}\n\n并把音乐代码粘贴在这里({shortcut})…"
 msg.SONG_IMPORTED = "音乐已载入：{title}."
 
+--- Play as a band
 msg.PLAY_IN_BAND = "乐队演奏"
 msg.PLAY_IN_BAND_HINT = "当你准备好和你的乐队一起演奏时，点击这里。"
 msg.PLAY_IN_BAND_READY_PLAYERS = "已就绪的乐队成员："
@@ -110,12 +134,14 @@ msg.EMOTE_PLAYER_IS_NOT_READY = "已离开就绪状态。"
 msg.EMOTE_PLAY_IN_BAND_START = "开始乐队演奏。"
 msg.EMOTE_PLAY_IN_BAND_STOP = "停止乐队演奏。"
 
+--- Play as a band (live)
 msg.LIVE_SYNC = "乐队现场演奏。"
 msg.LIVE_SYNC_HINT = "点击这里进行乐队演奏同步。"
 msg.SYNCED_PLAYERS = "就位的乐队成员："
 msg.EMOTE_PLAYER_LIVE_SYNC_ENABLED = "正在和你一起演奏音乐。"
 msg.EMOTE_PLAYER_LIVE_SYNC_DISABLED = "停止了和你演奏音乐。"
 
+--- Song editor frame
 msg.SONG_EDITOR = "音乐编辑器"
 msg.MARKER_FROM = "从"
 msg.MARKER_TO = "到"
@@ -137,6 +163,8 @@ msg.HEADER_NUMBER = "#"
 msg.HEADER_OCTAVE = "八度音阶"
 msg.HEADER_INSTRUMENT = "乐器"
 
+--- Configure live keyboard frame
+msg.SHOULD_CONFIGURE_KEYBOARD = "在开始演奏前需要对键盘进行设置。"
 msg.CONFIGURE_KEYBOARD = "设置键盘"
 msg.CONFIGURE_KEYBOARD_HINT = "点击一个键以设置……"
 msg.CONFIGURE_KEYBOARD_HINT_COMPLETE = "键盘设置已完成。\n你现在可以保存更改并开始演奏了！"
@@ -150,6 +178,7 @@ msg.KEY_CANNOT_BE_MERGED = "此时，请忽略并设置下一个键。"
 msg.NEXT_KEY = "下一个按键"
 msg.CLEAR_KEY = "清除按键"
 
+--- About frame
 msg.ABOUT_TITLE = "Musician"
 msg.ABOUT_VERSION = "版本 {version}"
 msg.ABOUT_AUTHOR = "By LenweSaralonde – {url}"
@@ -161,6 +190,7 @@ msg.ABOUT_PATREON = "成为Patreon捐助者：{url}"
 msg.ABOUT_PAYPAL = "成为PayPal捐助者：{url}"
 msg.ABOUT_SUPPORTERS = "特别鸣谢本插件的捐助者们！ <3"
 
+--- Fixed PC keyboard key names
 msg.FIXED_KEY_NAMES[KEY.Backspace] = "Backspace"
 msg.FIXED_KEY_NAMES[KEY.Tab] = "Tab"
 msg.FIXED_KEY_NAMES[KEY.CapsLock] = "Caps lock"
@@ -177,6 +207,7 @@ msg.FIXED_KEY_NAMES[KEY.ContextMenu] = "Menu"
 msg.FIXED_KEY_NAMES[KEY.ControlRight] = "Ctrl"
 msg.FIXED_KEY_NAMES[KEY.Delete] = "Delete"
 
+--- Live keyboard layouts, based on musical modes
 msg.KEYBOARD_LAYOUTS["Piano"] = "钢琴"
 msg.KEYBOARD_LAYOUTS["Chromatic"] = "半音阶"
 msg.KEYBOARD_LAYOUTS["Modes"] = "调式"
@@ -220,9 +251,11 @@ msg.KEYBOARD_LAYOUTS["4th"] = "纯四度"
 msg.KEYBOARD_LAYOUTS["5th"] = "纯五度"
 msg.KEYBOARD_LAYOUTS["Octave"] = "八度"
 
+--- Live keyboard layout types
 msg.HORIZONTAL_LAYOUT = "水平"
 msg.VERTICAL_LAYOUT = "垂直"
 
+--- Live keyboard frame
 msg.LIVE_SONG_NAME = "现场演奏"
 msg.SOLO_MODE = "独奏模式"
 msg.LIVE_MODE = "演奏模式"
@@ -256,15 +289,18 @@ msg.DEMO_MODE_ENABLED = "键盘演示模式启动：\n{mapping}"
 msg.DEMO_MODE_MAPPING = "{layer} → 音轨 #{track}"
 msg.DEMO_MODE_DISABLED = "键盘演示模式已关闭。"
 
+--- Live keyboard layers
 msg.LAYERS[Musician.KEYBOARD_LAYER.UPPER] = "上层"
 msg.LAYERS[Musician.KEYBOARD_LAYER.LOWER] = "下层"
 
+--- Chat emotes
 msg.EMOTE_PLAYING_MUSIC = "正在演奏一首音乐。"
 msg.EMOTE_PROMO = "(安装 \"Musician\" 插件就可以听了)"
 msg.EMOTE_SONG_NOT_LOADED = "(这首音乐无法播放，因为玩家 {player}使用的插件版本和你不兼容。)"
 msg.EMOTE_PLAYER_OTHER_REALM = "(这个玩家处于另一个服务器。)"
 msg.EMOTE_PLAYER_OTHER_FACTION = "(该玩家来自另一个阵营。)"
 
+--- Minimap button tooltips
 msg.TOOLTIP_LEFT_CLICK = "**左键点击**: {action}"
 msg.TOOLTIP_RIGHT_CLICK = "**右键点击**: {action}"
 msg.TOOLTIP_DRAG_AND_DROP = "**拖放** 来移动"
@@ -273,25 +309,30 @@ msg.TOOLTIP_ACTION_OPEN_MENU = "打开主菜单"
 msg.TOOLTIP_ACTION_MUTE = "静音所有音乐"
 msg.TOOLTIP_ACTION_UNMUTE = "取消静音"
 
+--- Player menu options
 msg.PLAYER_MENU_TITLE = "音乐"
 msg.PLAYER_MENU_STOP_CURRENT_SONG = "停止当前音乐"
 msg.PLAYER_MENU_MUTE = "静音"
 msg.PLAYER_MENU_UNMUTE = "取消静音"
 
+--- Player actions feedback
 msg.PLAYER_IS_MUTED = "{icon}{player}已静音。"
 msg.PLAYER_IS_UNMUTED = "{icon}{player}已关闭静音。"
 
+--- Song links
 msg.LINKS_PREFIX = "音乐"
 msg.LINKS_FORMAT = "{prefix}: {title}"
 msg.LINKS_LINK_BUTTON = "链接"
 msg.LINKS_CHAT_BUBBLE = "“{note}{title}”"
 
+--- Song link export frame
 msg.LINK_EXPORT_WINDOW_TITLE = "创建音乐链接"
 msg.LINK_EXPORT_WINDOW_SONG_TITLE_LABEL = "音乐标题："
 msg.LINK_EXPORT_WINDOW_HINT = "链接将在你退出登陆或重载界面前一直有效。"
 msg.LINK_EXPORT_WINDOW_PROGRESS = "正在创建链接…… {progress}%"
 msg.LINK_EXPORT_WINDOW_POST_BUTTON = "将链接粘贴到聊天框"
 
+--- Song link import frame
 msg.LINK_IMPORT_WINDOW_TITLE = "从玩家{player}导入音乐："
 msg.LINK_IMPORT_WINDOW_HINT = "点击“导入”以开始将音乐导入Musician。"
 msg.LINK_IMPORT_WINDOW_IMPORT_BUTTON = "导入音乐"
@@ -300,6 +341,7 @@ msg.LINK_IMPORT_WINDOW_REQUESTING = "正从玩家{player}请求数据……"
 msg.LINK_IMPORT_WINDOW_PROGRESS = "导入中……{progress}%"
 msg.LINK_IMPORT_WINDOW_SELECT_ACCOUNT = "请选择希望获取歌曲的角色："
 
+--- Song links errors
 msg.LINKS_ERROR.notFound = "音乐“{title}”无法从玩家{player}获取。"
 msg.LINKS_ERROR.alreadySending = "玩家{player}已经向你发送了一首音乐。请几秒后重试。"
 msg.LINKS_ERROR.alreadyRequested = "玩家{player}已经请求发送一首音乐。"
@@ -307,11 +349,13 @@ msg.LINKS_ERROR.timeout = "玩家{player}没有响应。"
 msg.LINKS_ERROR.offline = "{player}未登录魔兽世界。"
 msg.LINKS_ERROR.importingFailed = "歌曲{title}无法从玩家{player}处导入。"
 
-msg.MAP_OPTIONS_MAP = "地图"
+--- Map tracking options
+-- msg.MAP_OPTIONS_TITLE = "Map"
 msg.MAP_OPTIONS_SUB_TEXT = "显示附近在演奏的玩家："
 msg.MAP_OPTIONS_MINI_MAP = "于小地图显示"
 msg.MAP_OPTIONS_WORLD_MAP = "于世界地图显示"
 
+--- Total RP Extended module
 msg.TRPE_ITEM_NAME = "{title}"
 msg.TRPE_ITEM_TOOLTIP_REQUIRES_MUSICIAN = "需要Musician"
 msg.TRPE_ITEM_TOOLTIP_SHEET_MUSIC = "乐谱"
@@ -329,6 +373,7 @@ msg.TRPE_EXPORT_WINDOW_HINT_EXISTING = "已经存在一个这首音乐的物品�
 msg.TRPE_EXPORT_WINDOW_CREATE_ITEM_BUTTON = "创建物品"
 msg.TRPE_EXPORT_WINDOW_PROGRESS = "创建物品……{progress}%"
 
+--- Musician instrument names
 msg.INSTRUMENT_NAMES["none"] = "(无)"
 msg.INSTRUMENT_NAMES["accordion"] = "手风琴"
 msg.INSTRUMENT_NAMES["bagpipe"] = "风笛"
@@ -350,10 +395,12 @@ msg.INSTRUMENT_NAMES["distortion-guitar"] = "变音电吉他"
 msg.INSTRUMENT_NAMES["clean-guitar"] = "清音吉他"
 msg.INSTRUMENT_NAMES["bass-guitar"] = "贝斯吉他"
 msg.INSTRUMENT_NAMES["drumkit"] = "鼓组"
-msg.INSTRUMENT_NAMES['war-drum'] = "战鼓"
-msg.INSTRUMENT_NAMES['woodblock'] = "木块"
-msg.INSTRUMENT_NAMES['tambourine-shake'] = "铃鼓"
+msg.INSTRUMENT_NAMES["war-drum"] = "战鼓"
+msg.INSTRUMENT_NAMES["woodblock"] = "木块"
+msg.INSTRUMENT_NAMES["tambourine-shake"] = "铃鼓"
 
+--- General MIDI instrument names
+-- Check out Wikipedia in your own language to get the list.
 msg.MIDI_INSTRUMENT_NAMES[Instrument.AcousticGrandPiano] = "大钢琴"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.BrightAcousticPiano] = "亮音立式钢琴"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.ElectricGrandPiano] = "电声大钢琴"
@@ -483,6 +530,8 @@ msg.MIDI_INSTRUMENT_NAMES[Instrument.Helicopter] = "直升机声"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.Applause] = "欢呼鼓掌声"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.Gunshot] = "枪声"
 
+--- General MIDI drum kit names
+-- Check out Wikipedia in your own language to get the list.
 msg.MIDI_INSTRUMENT_NAMES[Instrument.StandardKit] = "标准鼓组"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.RoomKit] = "录音室鼓组"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.PowerKit] = "强音鼓组"
@@ -493,72 +542,72 @@ msg.MIDI_INSTRUMENT_NAMES[Instrument.BrushKit] = "鼓刷鼓组"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.OrchestraKit] = "管弦乐鼓组"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.SoundFXKit] = "声音效果"
 msg.MIDI_INSTRUMENT_NAMES[Instrument.MT32Kit] = "MT-32鼓组"
-
 msg.MIDI_INSTRUMENT_NAMES[Instrument.None] = "(无)"
-
 msg.UNKNOWN_DRUMKIT = "未知鼓组 ({midi})"
 
-msg.MIDI_PERCUSSION_NAMES[Percussion.Laser] = "激光"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Whip] = "鞭打"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ScratchPush] = "特效处理推音"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ScratchPull] = "特效处理拉音"
-msg.MIDI_PERCUSSION_NAMES[Percussion.StickClick] = "鼓槌对敲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SquareClick] = "敲方板"
-msg.MIDI_PERCUSSION_NAMES[Percussion.MetronomeClick] = "节拍器"
-msg.MIDI_PERCUSSION_NAMES[Percussion.MetronomeBell] = "节拍器重音"
-msg.MIDI_PERCUSSION_NAMES[Percussion.AcousticBassDrum] = "低音大鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.BassDrum1] = "高音大鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SideStick] = "鼓边"
-msg.MIDI_PERCUSSION_NAMES[Percussion.AcousticSnare] = "小鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HandClap] = "拍手声"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ElectricSnare] = "电子小鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowFloorTom] = "低音落地嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ClosedHiHat] = "合音踩镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HighFloorTom] = "高音落地嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.PedalHiHat] = "踏音踩镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowTom] = "低音嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.OpenHiHat] = "开音踩镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowMidTom] = "中低音嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HiMidTom] = "中高音嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.CrashCymbal1] = "低砸音镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HighTom] = "高音嗵鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.RideCymbal1] = "低浮音镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ChineseCymbal] = "中国镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.RideBell] = "浮音镲碗"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Tambourine] = "铃鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SplashCymbal] = "溅音镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Cowbell] = "牛铃"
-msg.MIDI_PERCUSSION_NAMES[Percussion.CrashCymbal2] = "高砸音镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Vibraslap] = "颤音叉"
-msg.MIDI_PERCUSSION_NAMES[Percussion.RideCymbal2] = "高浮音镲"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HiBongo] = "高音邦戈"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowBongo] = "低音邦戈"
-msg.MIDI_PERCUSSION_NAMES[Percussion.MuteHiConga] = "弱音康加"
-msg.MIDI_PERCUSSION_NAMES[Percussion.OpenHiConga] = "高音康加"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowConga] = "低音康加"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HighTimbale] = "高音铜鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowTimbale] = "低音铜鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HighAgogo] = "高音拉丁打铃"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowAgogo] = "低音拉丁打铃"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Cabasa] = "沙锤"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Maracas] = "响葫芦"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ShortWhistle] = "短哨"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LongWhistle] = "长哨"
-msg.MIDI_PERCUSSION_NAMES[Percussion.ShortGuiro] = "短锯琴"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LongGuiro] = "长锯琴"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Claves] = "击杆"
-msg.MIDI_PERCUSSION_NAMES[Percussion.HiWoodBlock] = "高音木块"
-msg.MIDI_PERCUSSION_NAMES[Percussion.LowWoodBlock] = "低音木块"
-msg.MIDI_PERCUSSION_NAMES[Percussion.MuteCuica] = "弱音吉加"
-msg.MIDI_PERCUSSION_NAMES[Percussion.OpenCuica] = "开音吉加"
-msg.MIDI_PERCUSSION_NAMES[Percussion.MuteTriangle] = "弱音三角铁"
-msg.MIDI_PERCUSSION_NAMES[Percussion.OpenTriangle] = "开音三角铁"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Shaker] = "沙锤（高音）"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SleighBell] = "马铃"
-msg.MIDI_PERCUSSION_NAMES[Percussion.BellTree] = "铃树"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Castanets] = "响板"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SurduDeadStroke] = "巴西鼓终音"
-msg.MIDI_PERCUSSION_NAMES[Percussion.Surdu] = "巴西鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SnareDrumRod] = "军鼓击杆"
-msg.MIDI_PERCUSSION_NAMES[Percussion.OceanDrum] = "拨浪鼓"
-msg.MIDI_PERCUSSION_NAMES[Percussion.SnareDrumBrush] = "军鼓擦杆"
+--- General MIDI percussion list
+-- Check out Wikipedia in your own language to get the list.
+msg.MIDI_PERCUSSION_NAMES[Percussion.Laser] = "激光" -- MIDI key 27
+msg.MIDI_PERCUSSION_NAMES[Percussion.Whip] = "鞭打" -- MIDI key 28
+msg.MIDI_PERCUSSION_NAMES[Percussion.ScratchPush] = "特效处理推音" -- MIDI key 29
+msg.MIDI_PERCUSSION_NAMES[Percussion.ScratchPull] = "特效处理拉音" -- MIDI key 30
+msg.MIDI_PERCUSSION_NAMES[Percussion.StickClick] = "鼓槌对敲" -- MIDI key 31
+msg.MIDI_PERCUSSION_NAMES[Percussion.SquareClick] = "敲方板" -- MIDI key 32
+msg.MIDI_PERCUSSION_NAMES[Percussion.MetronomeClick] = "节拍器" -- MIDI key 33
+msg.MIDI_PERCUSSION_NAMES[Percussion.MetronomeBell] = "节拍器重音" -- MIDI key 34
+msg.MIDI_PERCUSSION_NAMES[Percussion.AcousticBassDrum] = "低音大鼓" -- MIDI key 35
+msg.MIDI_PERCUSSION_NAMES[Percussion.BassDrum1] = "高音大鼓" -- MIDI key 36
+msg.MIDI_PERCUSSION_NAMES[Percussion.SideStick] = "鼓边" -- MIDI key 37
+msg.MIDI_PERCUSSION_NAMES[Percussion.AcousticSnare] = "小鼓" -- MIDI key 38
+msg.MIDI_PERCUSSION_NAMES[Percussion.HandClap] = "拍手声" -- MIDI key 39
+msg.MIDI_PERCUSSION_NAMES[Percussion.ElectricSnare] = "电子小鼓" -- MIDI key 40
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowFloorTom] = "低音落地嗵鼓" -- MIDI key 41
+msg.MIDI_PERCUSSION_NAMES[Percussion.ClosedHiHat] = "合音踩镲" -- MIDI key 42
+msg.MIDI_PERCUSSION_NAMES[Percussion.HighFloorTom] = "高音落地嗵鼓" -- MIDI key 43
+msg.MIDI_PERCUSSION_NAMES[Percussion.PedalHiHat] = "踏音踩镲" -- MIDI key 44
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowTom] = "低音嗵鼓" -- MIDI key 45
+msg.MIDI_PERCUSSION_NAMES[Percussion.OpenHiHat] = "开音踩镲" -- MIDI key 46
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowMidTom] = "中低音嗵鼓" -- MIDI key 47
+msg.MIDI_PERCUSSION_NAMES[Percussion.HiMidTom] = "中高音嗵鼓" -- MIDI key 48
+msg.MIDI_PERCUSSION_NAMES[Percussion.CrashCymbal1] = "低砸音镲" -- MIDI key 49
+msg.MIDI_PERCUSSION_NAMES[Percussion.HighTom] = "高音嗵鼓" -- MIDI key 50
+msg.MIDI_PERCUSSION_NAMES[Percussion.RideCymbal1] = "低浮音镲" -- MIDI key 51
+msg.MIDI_PERCUSSION_NAMES[Percussion.ChineseCymbal] = "中国镲" -- MIDI key 52
+msg.MIDI_PERCUSSION_NAMES[Percussion.RideBell] = "浮音镲碗" -- MIDI key 53
+msg.MIDI_PERCUSSION_NAMES[Percussion.Tambourine] = "铃鼓" -- MIDI key 54
+msg.MIDI_PERCUSSION_NAMES[Percussion.SplashCymbal] = "溅音镲" -- MIDI key 55
+msg.MIDI_PERCUSSION_NAMES[Percussion.Cowbell] = "牛铃" -- MIDI key 56
+msg.MIDI_PERCUSSION_NAMES[Percussion.CrashCymbal2] = "高砸音镲" -- MIDI key 57
+msg.MIDI_PERCUSSION_NAMES[Percussion.Vibraslap] = "颤音叉" -- MIDI key 58
+msg.MIDI_PERCUSSION_NAMES[Percussion.RideCymbal2] = "高浮音镲" -- MIDI key 59
+msg.MIDI_PERCUSSION_NAMES[Percussion.HiBongo] = "高音邦戈" -- MIDI key 60
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowBongo] = "低音邦戈" -- MIDI key 61
+msg.MIDI_PERCUSSION_NAMES[Percussion.MuteHiConga] = "弱音康加" -- MIDI key 62
+msg.MIDI_PERCUSSION_NAMES[Percussion.OpenHiConga] = "高音康加" -- MIDI key 63
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowConga] = "低音康加" -- MIDI key 64
+msg.MIDI_PERCUSSION_NAMES[Percussion.HighTimbale] = "高音铜鼓" -- MIDI key 65
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowTimbale] = "低音铜鼓" -- MIDI key 66
+msg.MIDI_PERCUSSION_NAMES[Percussion.HighAgogo] = "高音拉丁打铃" -- MIDI key 67
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowAgogo] = "低音拉丁打铃" -- MIDI key 68
+msg.MIDI_PERCUSSION_NAMES[Percussion.Cabasa] = "沙锤" -- MIDI key 69
+msg.MIDI_PERCUSSION_NAMES[Percussion.Maracas] = "响葫芦" -- MIDI key 70
+msg.MIDI_PERCUSSION_NAMES[Percussion.ShortWhistle] = "短哨" -- MIDI key 71
+msg.MIDI_PERCUSSION_NAMES[Percussion.LongWhistle] = "长哨" -- MIDI key 72
+msg.MIDI_PERCUSSION_NAMES[Percussion.ShortGuiro] = "短锯琴" -- MIDI key 73
+msg.MIDI_PERCUSSION_NAMES[Percussion.LongGuiro] = "长锯琴" -- MIDI key 74
+msg.MIDI_PERCUSSION_NAMES[Percussion.Claves] = "击杆" -- MIDI key 75
+msg.MIDI_PERCUSSION_NAMES[Percussion.HiWoodBlock] = "高音木块" -- MIDI key 76
+msg.MIDI_PERCUSSION_NAMES[Percussion.LowWoodBlock] = "低音木块" -- MIDI key 77
+msg.MIDI_PERCUSSION_NAMES[Percussion.MuteCuica] = "弱音吉加" -- MIDI key 78
+msg.MIDI_PERCUSSION_NAMES[Percussion.OpenCuica] = "开音吉加" -- MIDI key 79
+msg.MIDI_PERCUSSION_NAMES[Percussion.MuteTriangle] = "弱音三角铁" -- MIDI key 80
+msg.MIDI_PERCUSSION_NAMES[Percussion.OpenTriangle] = "开音三角铁" -- MIDI key 81
+msg.MIDI_PERCUSSION_NAMES[Percussion.Shaker] = "沙锤（高音）" -- MIDI key 82
+msg.MIDI_PERCUSSION_NAMES[Percussion.SleighBell] = "马铃" -- MIDI key 83
+msg.MIDI_PERCUSSION_NAMES[Percussion.BellTree] = "铃树" -- MIDI key 84
+msg.MIDI_PERCUSSION_NAMES[Percussion.Castanets] = "响板" -- MIDI key 85
+msg.MIDI_PERCUSSION_NAMES[Percussion.SurduDeadStroke] = "巴西鼓终音" -- MIDI key 86
+msg.MIDI_PERCUSSION_NAMES[Percussion.Surdu] = "巴西鼓" -- MIDI key 87
+msg.MIDI_PERCUSSION_NAMES[Percussion.SnareDrumRod] = "军鼓击杆" -- MIDI key 88
+msg.MIDI_PERCUSSION_NAMES[Percussion.OceanDrum] = "拨浪鼓" -- MIDI key 89
+msg.MIDI_PERCUSSION_NAMES[Percussion.SnareDrumBrush] = "军鼓擦杆" -- MIDI key 90
