5fd765b8f089be066ec2b4c53c1e625d86b045fe
LenweSaralonde <lenwe@lenwe.info>
2021-09-18 22:09:57 +0200

    Web converter version bump

--- MidiConvert/src/musician-midi-converter.js
+++ MidiConvert/src/musician-midi-converter.js
@@ -1,6 +1,6 @@
 const parseMidi = require('midi-file').parseMidi;
 
-export const CONVERTER_VERSION = '7.2';
+export const CONVERTER_VERSION = '7.3';
 
 const FILE_HEADER = 'MUS7';
 const MAX_NOTE_DURATION = 6;
