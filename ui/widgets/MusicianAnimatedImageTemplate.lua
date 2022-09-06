0aed07d1f8f7bf9d1b4c0c9748ad47b4da10e1ca
LenweSaralonde <lenwe@lenwe.info>
2022-09-06 23:42:24 +0200

    Moved XML LUA code to mixin

---
+++ ui/widgets/MusicianAnimatedImageTemplate.lua
@@ -0,0 +1,40 @@
+--- Animated image template mixin
+-- @module MusicianAnimatedImageTemplateMixin
+
+MusicianAnimatedImageTemplateMixin = {}
+
+--- OnLoad
+--
+function MusicianAnimatedImageTemplateMixin:OnLoad()
+	self.width = 1024
+	self.height = 1024
+	self.tileWidth = 128
+	self.tileHeight = 128
+	self.fps = 30
+end
+
+--- OnShow
+--
+function MusicianAnimatedImageTemplateMixin:OnShow()
+	self.frame = 0
+	self.elapsed = 0
+end
+
+--- OnUpdate
+-- @param elapsed (number)
+function MusicianAnimatedImageTemplateMixin:OnUpdate(elapsed)
+	local frameW = self.tileWidth / self.width
+	local frameH = self.tileHeight / self.height
+
+	local framesX = floor(self.width / self.tileWidth)
+	local framesY = floor(self.height / self.tileHeight)
+	local frames = framesX * framesY
+
+	self.elapsed = self.elapsed + elapsed
+	self.frame = floor(self.fps * (self.elapsed % (frames / self.fps)))
+
+	local row = math.floor(self.frame / framesX)
+	local col = self.frame % framesX
+
+	self.texture:SetTexCoord(col * frameW, (col + 1) * frameW, row * frameH, (row + 1) * frameH)
+end
