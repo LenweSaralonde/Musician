9e7f086e383024402de0796ee2a250155c43fa60
LenweSaralonde <lenwe@lenwe.info>
2022-09-08 20:39:25 +0200

    Moved widget LUA code out of the XML

---
+++ ui/widgets/MusicianEditBoxBackdropTemplate.lua
@@ -0,0 +1,22 @@
+--- EditBox backdrop template
+-- @module MusicianEditBoxBackdropTemplate
+
+--- OnLoad
+--
+function MusicianEditBoxBackdropTemplate_OnLoad(self)
+	self:SetBackdrop({
+		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+		tile = true,
+		tileSize = 16,
+		edgeSize = 16,
+		insets = {
+			left = 5,
+			right = 5,
+			top = 5,
+			bottom = 5,
+		},
+	})
+	self:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b)
+	self:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b)
+end
