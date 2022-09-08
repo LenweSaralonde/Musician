ed357bf2f8cd556f0726f6bdf46378d1b4b64eb6
LenweSaralonde <lenwe@lenwe.info>
2022-09-08 18:38:57 +0200

    Moved XML LUA code to mixin

--- ui/widgets/MusicianDialogTemplate.lua
+++ ui/widgets/MusicianDialogTemplate.lua
@@ -12,3 +12,46 @@ function MusicianDialogTemplateMixin:Toggle()
 		self:Show()
 	end
 end
+
+--- OnLoad
+--
+function MusicianDialogTemplateMixin:OnLoad()
+	Musician.RestoreFramePosition(self)
+	self:RegisterForDrag("LeftButton")
+end
+
+--- OnDragStart
+--
+function MusicianDialogTemplateMixin:OnDragStart()
+	self:StartMoving()
+end
+
+--- OnDragStop
+--
+function MusicianDialogTemplateMixin:OnDragStop()
+	self:StopMovingOrSizing()
+	Musician.SaveFramePosition(self)
+end
+
+--- OnKeyDown
+-- @param key (string)
+function MusicianDialogTemplateMixin:OnKeyDown(key)
+	if self:IsShown() and key == "ESCAPE" and not(self.noEscape) then
+		self:SetPropagateKeyboardInput(false)
+		self:Hide()
+	else
+		self:SetPropagateKeyboardInput(true)
+	end
+end
+
+--- OnShow
+--
+function MusicianDialogTemplateMixin:OnShow()
+	PlaySound(SOUNDKIT.IG_QUEST_LIST_OPEN)
+end
+
+--- OnHide
+--
+function MusicianDialogTemplateMixin:OnHide()
+	PlaySound(SOUNDKIT.IG_QUEST_LIST_CLOSE)
+end
