da4bb9745f41d936a8fc8e11dcb904d3cf035943
LenweSaralonde <lenwe@lenwe.info>
2022-09-15 19:49:46 +0200

    Added tooltipText support for check buttons

--- ui/widgets/MusicianCheckButtonTemplates.lua
+++ ui/widgets/MusicianCheckButtonTemplates.lua
@@ -20,6 +20,24 @@ function MusicianCheckButtonTemplate_OnDisable(self)
 	self.Text:SetTextColor(GRAY_FONT_COLOR.r, GRAY_FONT_COLOR.g, GRAY_FONT_COLOR.b)
 end
 
+--- OnEnter
+--
+function MusicianCheckButtonTemplate_OnEnter(self)
+	if(self.tooltipText ~= nil) then
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip_SetTitle(GameTooltip, self.tooltipText)
+		GameTooltip:Show()
+	end
+end
+
+--- OnLeave
+--
+function MusicianCheckButtonTemplate_OnLeave(self)
+	if(self.tooltipText ~= nil) then
+		GameTooltip:Hide()
+	end
+end
+
 -- Hack to set an alias for addon backward compatibility.
 function MusicianCheckButtonTemplate_SetParentKeyAlias(fontString)
 	local parent = fontString:GetParent()
